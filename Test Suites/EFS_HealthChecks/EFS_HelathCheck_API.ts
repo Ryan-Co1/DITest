e189d53e7c54a1ba90b898a63fca34ba4696b5fc
Alekhya.Vallabhaneni <Alekhya.Vallabhaneni@INLT30072.ert.com>
2020-03-12 05:34:37 -0400

    EFS Test suite

---
+++ Test Suites/EFS_HealthChecks/EFS_HelathCheck_API.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Verifies health and ping APIs</description>
+   <name>EFS_HelathCheck_API</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f438ae65-6a13-4b72-b376-d906f111a7fa</testSuiteGuid>
+   <testCaseLink>
+      <guid>ca3bec40-7009-4e44-ac26-cadcb4ed407a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EFS_HealthCheck_health_DI-15_V1.0</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>797d5272-37b6-43e3-a582-3c7cb7981d6e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EFS_HealthCheck_Ping_DI14_V1.0</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
